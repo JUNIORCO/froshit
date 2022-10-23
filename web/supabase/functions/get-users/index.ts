815e8ee53025f970a25f7b4c195715aac917803c
Sami Junior <sami.jr13@hotmail.com>
2022-10-23 18:36:25 -0400

    testing new folder structure

---
+++ web/supabase/functions/get-users/index.ts
@@ -0,0 +1,37 @@
+// @ts-nocheck
+import { prisma } from '../../../prisma';
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts';
+
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+serve(async (req: Request) => {
+  try {
+    console.log('user function is called...');
+    const users = prisma.profile.findMany({
+      include: {
+        frosh: true,
+        team: true,
+      },
+    });
+
+    console.log('user function get-users : ', users);
+
+    return new Response(JSON.stringify({ data: users, error }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    });
+  }
+});
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/create-froshee' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"World"}'
