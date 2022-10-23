23c4ebd0b93766a8d36452592c54dac45a196727
Sami Junior <sami.jr13@hotmail.com>
2022-10-23 19:25:47 -0400

    test this

--- mobile/supabase/supabase.ts
+++ mobile/supabase/supabase.ts
@@ -3,17 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 
 import { SUPABASE_PUBLIC_KEY, SUPABASE_URL } from '@env';
 
-console.log('SUPABASE_URL : ', SUPABASE_URL)
-console.log('SUPABASE_PUBLIC_KEY : ', SUPABASE_PUBLIC_KEY)
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-  db: {
-    schema: 'public',
-  },
-});
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+    db: {
+      schema: 'public',
+    },
+  });
