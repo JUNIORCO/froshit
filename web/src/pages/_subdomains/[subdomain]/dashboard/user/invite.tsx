071ec530d092e56ca88fce45095bbde2344728a4
Sami Junior <sami.jr13@hotmail.com>
2022-12-15 08:12:11 +0200

    reset env variables

--- web/src/pages/_subdomains/[subdomain]/dashboard/user/invite.tsx
+++ web/src/pages/_subdomains/[subdomain]/dashboard/user/invite.tsx
@@ -48,6 +48,7 @@ const sendInviteRequest = async (url: string, { arg }: any) => {
   return res.json();
 };
 
+
 UserInvite.getLayout = function getLayout(page: React.ReactElement) {
   return <Layout>{page}</Layout>;
 };
