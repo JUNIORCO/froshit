2398e9d9f0430a5819eb71d85be69e30f3cf9ea2
Sami Junior <sami.jr13@hotmail.com>
2023-01-09 22:12:05 -0500

    move logo to assets

--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -16,8 +16,7 @@ const Logo = forwardRef<any, Props>(({ university = false, disabledLink = false,
   const supabaseClient = useSupabaseClient();
   const { subdomain } = useSubdomain();
 
-  // const { data: { publicUrl: froshitLogo } } = supabaseClient.storage.from('froshit').getPublicUrl('logo.svg');
-  const froshitLogo = 'https://mybvkrkmvnuzeqvzgbzg.supabase.co/storage/v1/object/public/froshit/logo.webp';
+  const froshitLogo = 'assets/logo.webp';
   const [displayedLogo, setDisplayedLogo] = useState<string | null>();
 
   const universityLogoFromProfile = profile?.university?.imageUrl;
