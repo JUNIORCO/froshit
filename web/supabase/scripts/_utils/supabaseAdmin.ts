d02e698e0bab9e3a0790dbd4be933e2afc775c2a
Sami Junior <sami.jr13@hotmail.com>
2023-03-17 15:48:59 -0400

    .env gitignore

--- web/supabase/scripts/_utils/supabaseAdmin.ts
+++ web/supabase/scripts/_utils/supabaseAdmin.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseAdmin = createClient(
-  'https://mybvkrkmvnuzeqvzgbzg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15YnZrcmttdm51emVxdnpnYnpnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NDA1OTExNiwiZXhwIjoxOTc5NjM1MTE2fQ.4KMplC7mH59RjXzFmAtuFpoT081C9NpwCjVE4otWaKM');
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY ?? '',
+);
